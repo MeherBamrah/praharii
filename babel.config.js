58a26f0bf2e771e138d8c3c32dd02fa3d0f35076
Meher_Codez <156418981+MeherBamrah@users.noreply.github.com>
2026-06-05 12:59:10 +0530

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,9 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      [
+     [
+        'react-native-reanimated/plugin',
+        ['react-native-worklets-core/plugin'],
         'module-resolver',
         {
           root: ['./'],
